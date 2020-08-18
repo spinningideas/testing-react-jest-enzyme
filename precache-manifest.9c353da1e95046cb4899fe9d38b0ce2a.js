c3fc7bc7ab6bff103dd44db5410e202240f38fda
spinningideas <scott@spinningideas.com>
2020-08-17 20:25:59 -0600

    Updates

---
+++ precache-manifest.9c353da1e95046cb4899fe9d38b0ce2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb32fc5290eea615941ece8db445264",
+    "url": "https://spinningideas.github.io/testing-react-jest-enzyme/index.html"
+  },
+  {
+    "revision": "98322e00107a0fee9040",
+    "url": "https://spinningideas.github.io/testing-react-jest-enzyme/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "689c65823c9fcbc7713a",
+    "url": "https://spinningideas.github.io/testing-react-jest-enzyme/static/css/main.8bde5e94.chunk.css"
+  },
+  {
+    "revision": "98322e00107a0fee9040",
+    "url": "https://spinningideas.github.io/testing-react-jest-enzyme/static/js/2.e7276477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://spinningideas.github.io/testing-react-jest-enzyme/static/js/2.e7276477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689c65823c9fcbc7713a",
+    "url": "https://spinningideas.github.io/testing-react-jest-enzyme/static/js/main.47945ec0.chunk.js"
+  },
+  {
+    "revision": "8c5cc7d799cdf8b81468",
+    "url": "https://spinningideas.github.io/testing-react-jest-enzyme/static/js/runtime-main.7b3dd61f.js"
+  }
+]);
